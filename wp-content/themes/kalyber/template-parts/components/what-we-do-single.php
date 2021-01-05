d6185d809c878707b211b8a864691651c64c3eed
Todd Gimlin <tgimlin@gmail.com>
2021-01-05 12:10:25 -0600

    what we do single page styles

--- wp-content/themes/kalyber/template-parts/components/what-we-do-single.php
+++ wp-content/themes/kalyber/template-parts/components/what-we-do-single.php
@@ -10,22 +10,24 @@
           <?= $content ?>
      </div>
      <div class="what-we-do-single__form">
-          <h4>Lorem ipsum dolor sit amet consectetur.</h4>
+          <h3 class="color__white">Get in Touch</h3>
+          <p class="color__white">For more information about how Kalyber can help your business, use the form below.</p>
+          <br>
           <form class="go-rightx">
-               <label for="first name">First Name</label>
+               <label for="first name">First Name*</label>
                <input id="first_name" name="first name" type="text" required>
 
-               <label for="last name">Last Name</label>
+               <label for="last name">Last Name*</label>
                <input id="last_name" name="last name" type="text" required>
 
-               <label for="phone">Phone</label>
-               <input id="phone" name="phone" type="tel" required>
+               <label for="email">Email*</label>
+               <input id="email" name="email" type="email" required>
 
-               <label for="company">Company</label>
-               <input id="company" name="company" type="tel" required>
+               <label for="website">Website URL*</label>
+               <input id="website" name="company" type="tel" required>
 
-               <label for="message">Message</label>
-               <textarea id="message" name="message" required></textarea>
+               <label for="comments">Notes and Comments</label>
+               <textarea id="comments" name="comments" required></textarea>
 
                <input type="submit">
           </form>
