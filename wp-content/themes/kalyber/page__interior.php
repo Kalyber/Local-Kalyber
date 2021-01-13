e35566f6fbad0c87b1c873d4907b0448d31a76ca
Todd Gimlin <tgimlin@gmail.com>
2021-01-13 09:38:09 -0600

    footer, page template changes

--- wp-content/themes/kalyber/page__interior.php
+++ wp-content/themes/kalyber/page__interior.php
@@ -3,7 +3,13 @@
 get_header();?>
 
 <?php get_template_part('template-parts/components/hero-interior'); ?>
-<span style="font-size:200px;">Hello</span>
-<?php //get_template_part('template-parts/components/landing-page-content'); ?>
+
+<div class="landing">
+  <div class="landing_inner_wrap">
+    <div class="landing__rich-text--wrap">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer();?>
