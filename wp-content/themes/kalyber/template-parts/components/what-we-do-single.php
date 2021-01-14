fc26e3c2874e5dc612c0127aa114a41490871f9a
Todd Gimlin <tgimlin@gmail.com>
2021-01-14 15:15:43 -0600

    changes to services landing sidebar

--- wp-content/themes/kalyber/template-parts/components/what-we-do-single.php
+++ wp-content/themes/kalyber/template-parts/components/what-we-do-single.php
@@ -12,24 +12,21 @@
      <div class="what-we-do-single__form">
           <h3 class="color__white">Supercharge Your Marketing</h3>
           <p class="color__white">For more information about how Kalyber can help your business, use the form below.</p>
-          <br>
-          <form class="go-rightx">
+
+          <iframe src="https://go.kalyber.com/l/869051/2021-01-14/71q6l9" width="100%" height="1000" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
+
+          <!-- <form class="go-rightx">
                <label for="first name">First Name*</label>
                <input id="first_name" name="first name" type="text" required>
-
                <label for="last name">Last Name*</label>
                <input id="last_name" name="last name" type="text" required>
-
                <label for="email">Email*</label>
                <input id="email" name="email" type="email" required>
-
                <label for="website">Website URL*</label>
                <input id="website" name="company" type="tel" required>
-
                <label for="comments">Notes and Comments</label>
                <textarea id="comments" name="comments" required></textarea>
-
                <input type="submit">
-          </form>
+          </form> -->
      </div>
 </article>
