85711c1ea151b96c589795cb101aa29459b43704
Nick Giusti <nick@Nicks-MacBook-Air.local>
2021-01-15 12:49:43 -0800

    updated masonry page to use featured image instead of banner

--- wp-content/themes/kalyberOLD/template-parts/section-content.php
+++ wp-content/themes/kalyberOLD/template-parts/section-content.php
@@ -1,6 +1,12 @@
 
+<?php     
+     $featured_id=get_post_thumbnail_id();
+     $featured_url_array=wp_get_attachment_image_src($featured_id, 'thumbnail-size', true);
+     $featured_url=$featured_url_array[0]; 
+?>
 
-<a href="<?php the_permalink();?>" class="item" style="background-image: url('<?php the_field('banner_image'); ?>');">
+
+<a href="<?php the_permalink();?>" class="item" style="background-image: url(<?= $featured_url ?>);">
      <div class="item__details">
           <h5><?php the_field('title'); ?></h5>
           <span class="read">Read More</span>
