966e31b7ce2895ae958d9d5ec7258c2379052746
Todd Gimlin <tgimlin@gmail.com>
2021-01-17 16:08:56 -0600

    style changes throughout

--- wp-content/themes/kalyber/single.php
+++ wp-content/themes/kalyber/single.php
@@ -17,23 +17,14 @@ get_header();
     $featured_url = $featured_url_array[0];    
 ?>
 
-
 <main class="blog">
     <div class="blog__inner__wrap">
         <div class="blog__hero__wrap">
             <div class="blog__hero-image" style="background-image: url(<?= $featured_url ?>)"></div>
             <div class="blog__hero-text__wrap">
                 <h1><?php the_field('title'); ?></h1>
-                <p><?php the_field('subheader'); ?></p>
             </div>
         </div>
-
-        <!-- <a href="javascript:history.back()">
-        <div class="blog__back-button__wrap">
-            BACK
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M17.026 22.957c10.957-11.421-2.326-20.865-10.384-13.309l2.464 2.352h-9.106v-8.947l2.232 2.229c14.794-13.203 31.51 7.051 14.794 17.675z"/></svg>
-        </div>
-        </a> -->
 
         <div class="blog__rich-text">
             <div class="blog__rich-text--wrap">
@@ -43,15 +34,11 @@ get_header();
             
             <?php get_template_part('template-parts/pop-up'); ?>
         </div> 
-        
 
         <?php //get_template_part('template-parts/section','relatedposts'); ?>
        
     </div>
-
 </main>
-
-
 
 <?php endwhile; else: endif; ?>
 
--- wp-content/themes/kalyber/template-parts/components/contact-form.php
+++ wp-content/themes/kalyber/template-parts/components/contact-form.php
@@ -11,13 +11,15 @@
 
                <hr>
 
-               <p><strong>Email</strong> <a href="mailto:hello@kalyber.com">hello@kalyber.com</a></p>
-               <p><strong>Phone</strong> 888.525.9237</p>
+               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><title>Email Us</title><path d="M12 .02c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.99 6.98l-6.99 5.666-6.991-5.666h13.981zm.01 10h-14v-8.505l7 5.673 7-5.672v8.504z"/></svg>
+               <a href="mailto:hello@kalyber.com">hello@kalyber.com</a></p>
+
+
+               <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><title>Call Us</title><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3.445 17.827c-3.684 1.684-9.401-9.43-5.8-11.308l1.053-.519 1.746 3.409-1.042.513c-1.095.587 1.185 5.04 2.305 4.497l1.032-.505 1.76 3.397-1.054.516z"/></svg>
+               <a>888.525.9237</a>
           </div>
           <div class="contact-form__form">
-
                <iframe src="https://go.kalyber.com/l/869051/2020-06-01/71btl6?parentUrl=<?= $hostUrl ?>" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
-
           </div>
      </div>
 </div>
