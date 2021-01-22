11c5110eb92de89a4fd16248d5e06fe4ade8fea8
Todd Gimlin <tgimlin@gmail.com>
2021-01-22 16:07:32 -0600

    header change

--- wp-content/themes/kalyber/header.php
+++ wp-content/themes/kalyber/header.php
@@ -15,8 +15,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<title><?php wp_title(''); ?></title>
-
 	<?php if (is_search()) { ?>
 		<meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
